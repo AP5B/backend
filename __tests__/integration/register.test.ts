c4ef95ad5816a59b35043f553ed523fee903c4da
viceret <vpretamal@uc.cl>
2025-12-04 17:11:20 -0300

    test de register corregido

--- __tests__/integration/register.test.ts
+++ __tests__/integration/register.test.ts
@@ -162,7 +162,7 @@ describe("Register endpoints", () => {
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty(
         "message",
-        "El nombre de usuario debe tener entre 5 y 20 caracteres.",
+        "El nombre de usuario debe tener entre 3 y 20 caracteres.",
       );
     });
 
@@ -185,7 +185,7 @@ describe("Register endpoints", () => {
       expect(res.status).toBe(400);
       expect(res.body).toHaveProperty(
         "message",
-        "El nombre de usuario debe tener entre 5 y 20 caracteres.",
+        "El nombre de usuario debe tener entre 3 y 20 caracteres.",
       );
     });
 
